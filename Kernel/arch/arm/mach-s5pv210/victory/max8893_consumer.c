e7387f03f6a2d07af17938455f96729a65d3f4ba
Randy Terbush <randy@terbush.org>
2011-04-20 23:13:40 -0600

    Could not take it any longer. Reformated using scripts/Lindent to get these back to consistent coding style.

--- Kernel/arch/arm/mach-s5pv210/victory/max8893_consumer.c
+++ Kernel/arch/arm/mach-s5pv210/victory/max8893_consumer.c
@@ -32,58 +32,56 @@
 #include <linux/regulator/max8893.h>
 #include <linux/regulator/consumer.h>
 
-
-
 #define DBG(fmt...)
 //#define DBG printk
 
-#if 1 //victory.rev 02 (temp_pfe)
+#if 1				//victory.rev 02 (temp_pfe)
 extern int hw_rev_adc;
 #endif
 
 static int max8893_consumer_probe(struct platform_device *pdev)
 {
-    int ret = 0;
+	int ret = 0;
 
-	DBG("func =%s \n",__func__);	
+	DBG("func =%s \n", __func__);
 
-	if(1150 <= hw_rev_adc){
-      max8893_ldo_disable_direct(MAX8893_BUCK);
-      DBG("max8893 Buck is disabled\n");        
+	if (1150 <= hw_rev_adc) {
+		max8893_ldo_disable_direct(MAX8893_BUCK);
+		DBG("max8893 Buck is disabled\n");
 	}
 
-	return ret;	
+	return ret;
 }
 
 static int max8893_consumer_remove(struct platform_device *pdev)
 {
-    int ret = 0;
+	int ret = 0;
 
-	DBG("func =%s \n",__func__);	
-	
+	DBG("func =%s \n", __func__);
+
 	return ret;
 
 }
 
-static int max8893_consumer_suspend(struct platform_device *dev, pm_message_t state)
+static int max8893_consumer_suspend(struct platform_device *dev,
+				    pm_message_t state)
 {
 	int i, pmic_controling_list;
 
-	DBG("func =%s \n",__func__);	
+	DBG("func =%s \n", __func__);
 
-	if(hw_rev_adc <= 995){ 
-	    max8893_ldo_disable_direct(MAX8893_BUCK);
-      //max8893_ldo_set_voltage_direct(MAX8893_BUCK,1800000,1800000);
-      DBG("max8893 Buck is disabled\n");        
+	if (hw_rev_adc <= 995) {
+		max8893_ldo_disable_direct(MAX8893_BUCK);
+		//max8893_ldo_set_voltage_direct(MAX8893_BUCK,1800000,1800000);
+		DBG("max8893 Buck is disabled\n");
 	}
-   
 	//max8893_ldo_disable_direct(MAX8893_BUCK);
 	//max8893_ldo_disable_direct(MAX8893_LDO1);
 	//max8893_ldo_disable_direct(MAX8893_LDO2);
 	//max8893_ldo_disable_direct(MAX8893_LDO3);
 	//max8893_ldo_disable_direct(MAX8893_LDO4);
-	//max8893_ldo_disable_direct(MAX8893_LDO5);	
-	
+	//max8893_ldo_disable_direct(MAX8893_LDO5);     
+
 	return 0;
 }
 
@@ -91,27 +89,26 @@ static int max8893_consumer_resume(struct platform_device *dev)
 {
 	int i, saved_control;
 
-	DBG("func =%s \n",__func__);	
+	DBG("func =%s \n", __func__);
 
-  max8893_ldo_set_voltage_direct(MAX8893_LDO1,2900000,2900000);
-  max8893_ldo_set_voltage_direct(MAX8893_LDO2,3000000,3000000); //20100628_inchul(from HW)
-  max8893_ldo_set_voltage_direct(MAX8893_LDO3,3000000,3000000);
-  max8893_ldo_set_voltage_direct(MAX8893_LDO4,3000000,3000000);
-  max8893_ldo_set_voltage_direct(MAX8893_LDO5,1800000,1800000);
-  max8893_ldo_set_voltage_direct(MAX8893_BUCK,1800000,1800000);
+	max8893_ldo_set_voltage_direct(MAX8893_LDO1, 2900000, 2900000);
+	max8893_ldo_set_voltage_direct(MAX8893_LDO2, 3000000, 3000000);	//20100628_inchul(from HW)
+	max8893_ldo_set_voltage_direct(MAX8893_LDO3, 3000000, 3000000);
+	max8893_ldo_set_voltage_direct(MAX8893_LDO4, 3000000, 3000000);
+	max8893_ldo_set_voltage_direct(MAX8893_LDO5, 1800000, 1800000);
+	max8893_ldo_set_voltage_direct(MAX8893_BUCK, 1800000, 1800000);
 
-	if(hw_rev_adc <= 995){
-	    max8893_ldo_enable_direct(MAX8893_BUCK);
-      DBG("max8893 Buck is enabled\n");        
+	if (hw_rev_adc <= 995) {
+		max8893_ldo_enable_direct(MAX8893_BUCK);
+		DBG("max8893 Buck is enabled\n");
 	}
-
 	//max8893_ldo_enable_direct(MAX8893_BUCK);
 	//max8893_ldo_enable_direct(MAX8893_LDO1);
 	//max8893_ldo_enable_direct(MAX8893_LDO2);
 	//max8893_ldo_enable_direct(MAX8893_LDO3);
 	//max8893_ldo_enable_direct(MAX8893_LDO4);
-	//max8893_ldo_enable_direct(MAX8893_LDO5);	
-	
+	//max8893_ldo_enable_direct(MAX8893_LDO5);      
+
 	return 0;
 }
 
@@ -122,8 +119,8 @@ static struct platform_driver max8893_consumer_driver = {
 		   },
 	.probe = max8893_consumer_probe,
 	.remove = max8893_consumer_remove,
-    .suspend = max8893_consumer_suspend, 
-    .resume = max8893_consumer_resume,
+	.suspend = max8893_consumer_suspend,
+	.resume = max8893_consumer_resume,
 
 };
 
@@ -131,12 +128,14 @@ static int max8893_consumer_init(void)
 {
 	return platform_driver_register(&max8893_consumer_driver);
 }
+
 late_initcall(max8893_consumer_init);
 
 static void max8893_consumer_exit(void)
 {
 	platform_driver_unregister(&max8893_consumer_driver);
 }
+
 module_exit(max8893_consumer_exit);
 
 MODULE_AUTHOR("Amit Daniel");
